a7862a125822945e1f1848695baf71551420b9db
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2018-08-08 08:13:18 -0500

    Include ex_doc

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Slax.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:erlsom, "~> 1.4"}
+      {:erlsom, "~> 1.4"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
