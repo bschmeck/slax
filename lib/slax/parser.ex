b9621c1f5fc1e56896c6427c5dd793557b77123e
Hendrik Richter <info@prokapi.com>
2020-05-13 16:31:00 +0200

    fix behaviour for Parser.handle/2

--- lib/slax/parser.ex
+++ lib/slax/parser.ex
@@ -1,7 +1,9 @@
 defmodule Slax.Parser do
+  alias Slax.Event.{Characters, EndDocument, EndElement, EndPrefixMapping, Error, IgnorableWhitespace, InternalError, ProcessingInstruction, StartDocument, StartElement, StartPrefixMapping}
+
   @callback init(arg :: any) :: any
   @callback finalize(arg :: any) :: any
-  @callback handle(tuple(), arg :: any) :: any
+  @callback handle(arg :: (Characters.t | EndDocument.t | EndElement.t | EndPrefixMapping.t | Error.t | IgnorableWhitespace.t | InternalError.t | ProcessingInstruction.t | StartDocument.t | StartElement.t | StartPrefixMapping.t), arg :: any) :: any
   @optional_callbacks init: 1
 
   defmacro __using__(opts \\ []) do
